bbe3eaaa2b07d74c8b4391512fd1c15f01fdfc6b
MaxWofford <max.speed.wofford@gmail.com>
2014-08-23 21:56:45 -0700

    Added placeholder info

--- interface.js
+++ interface.js
@@ -5,18 +5,58 @@ function toggleOptions() {
 		document.getElementById('optionsHider').className = '';
 	};
 }
+
 function hideInfo() {
 	document.getElementById('sideBarHider').className = 'hidden';
 }
-function clickInfo(planetId) {
-	document.getElementById('sideBarHider').className = '';
-	document.getElementById('planetIdInfo').innerText = planetId;
-	document.getElementById('flyToButton').onclick = function() {verifyFly(planetId)};
-}
+
 
 function verifyFly(planetId) {
 	document.getElementById('verifyFlyHider').className = '';
 	console.log(planetId);
 	document.getElementById('planetId').innerText = planetId;
 	//fly(planetId, currentPlanet);
+}
+
+function clickInfo(planetId) {
+	document.getElementById('sideBarHider').className = '';
+	document.getElementById('flyToButton').onclick = function() {verifyFly(planetId)};
+
+switch (planetId) {
+  	case 0:
+  	document.getElementById('planetIdInfo').innerText = "Earth";
+    document.getElementById('infoProduces').innerText = "Bad videogames made in a day";
+    document.getElementById('infoReputation').innerText = "Placeholder";
+    	break;
+  	case 1:
+  	document.getElementById('planetIdInfo').innerText = "Mars";
+    document.getElementById('infoProduces').innerText = "red dust";
+    document.getElementById('infoReputation').innerText = "Placeholder";
+    	break;
+    case 2:
+  	document.getElementById('planetIdInfo').innerText = "Asteroid";
+    document.getElementById('infoProduces').innerText = "Placeholder";
+    document.getElementById('infoReputation').innerText = "Placeholder";
+    	break;
+    case 3:
+  	document.getElementById('planetIdInfo').innerText = "Jupiter";
+    document.getElementById('infoProduces').innerText = "Placeholder";
+    document.getElementById('infoReputation').innerText = "Placeholder";
+    	break;
+    case 4:
+  	document.getElementById('planetIdInfo').innerText = "Saturn";
+    document.getElementById('infoProduces').innerText = "Placeholder";
+    document.getElementById('infoReputation').innerText = "Placeholder";
+    	break;
+    case 5:
+  	document.getElementById('planetIdInfo').innerText = "Netptun";
+    document.getElementById('infoProduces').innerText = "";
+    document.getElementById('infoReputation').innerText = "";
+    	break;
+  	default:
+  	document.getElementById('planetIdInfo').innerText = "Uranus";
+    document.getElementById('infoProduces').innerText = "Methane :p";
+    document.getElementById('infoReputation').innerText = "";
+    	break;
+	}
 }
