6a46d87d73e2acd1e778a6e6bad96658fedff1ef
Benjamin Landers <benjaminrlanders@gmail.com>
2014-08-23 22:24:35 -0700

    added gamestate

---
+++ gameplay.js
@@ -0,0 +1,40 @@
+var gamestate = {};
+gamestate.planetData = [];
+gamestate.planetData[0] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.planetData[1] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.planetData[2] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.planetData[3] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.planetData[4] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.planetData[5] = {name:"world",technology:0,minerals:0,fuel:0,food:0,antimatter:0,production:0,society:0};
+gamestate.currentPlanet = 0;
+
+function generateWorlds(){
+
+}
+
+function timeStep(){
+
+
+}
+
+function generateCosts(planet){
+
+}
+
+function generateDemand(planet){
+
+
+}
+
+function generateSupply(planet){
+
+
+}
+
+gamestate.playerData = {reputation:[0,0,0,0,0,0]};
+gamestate.playerData.inventory = {};
+gamestate.playerData.ship = {engine:{},cargobay:{}};
+
+function jump(to,from){
+
+}
